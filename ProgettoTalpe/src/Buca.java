559fcd2382ef5b76d34eec2895f997033b01ab6e
leobracca <braccalenti.leonardo@franchettisalviani.net>
2026-03-10 17:00:48 +0100

    Fatta la logica della buca
    -Import e attributi utili per l'implementazione
    -Thread che viene attivato quando si preme il bottone, si aggiorna il punteggio e la talpa scompare

--- ProgettoTalpe/src/Buca.java
+++ ProgettoTalpe/src/Buca.java
@@ -2,11 +2,30 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
+import javax.swing.*;
 /**
  *
  * @author lbrac
  */
-public class Buca {
+public class Buca extends JButton{
+    boolean talpa = false;
+    ImageIcon iBuca = new ImageIcon(getClass().getResource("/buca.png"));
+    ImageIcon iTalpa = new ImageIcon(getClass().getResource("/talpa.png"));
     
+    
+    Buca(LogicaGioco logica){
+        this.setIcon(iBuca);
+        
+        this.addActionListener(e ->{
+            if(talpa == true){
+                logica.addPunti();
+                setStato(false);
+            }
+        });
+    }
+    
+    void setStato(boolean attiva){
+        this.talpa = attiva;
+        this.setIcon(attiva ? iTalpa : iBuca);
+    }
 }
